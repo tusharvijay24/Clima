0d983d143eacbfe2663f18e2685122f439583dc0
Tushar Vijayvargiya <65571665+tusharvijay24@users.noreply.github.com>
2021-06-30 00:33:00 +0530

    Add files via upload

---
+++ Clima/Clima/Models/WeatherData.swift
@@ -0,0 +1,21 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by Tushar Vijayvargiya on 29/06/21.
+//
+
+import Foundation
+struct WeatherData: Codable {
+    let name: String
+    let main: Main
+    let weather:[Weather]
+}
+struct Main: Codable
+{
+    let temp: Double
+}
+struct Weather: Codable {
+    let description: String
+    let id : Int
+}
